7d343e3e5c88216112d55b5f1611a90ece49eac3
Pol Lamothe <lamothepol@gmail.com>
2023-11-24 23:17:02 +0100

    Ajout de l'extension camera fluide

--- game/draw.go
+++ game/draw.go
@@ -1,5 +1,6 @@
 package game
 
+import "C"
 import (
 	"fmt"
 	"image/color"
@@ -16,9 +17,8 @@ import (
 // Il faut faire attention à l'ordre d'affichage pour éviter d'avoir
 // des éléments qui en cachent d'autres.
 func (g *Game) Draw(screen *ebiten.Image) {
-	g.floor.Draw(screen)
-	g.character.Draw(screen, g.camera.X, g.camera.Y)
-
+	g.floor.Draw(screen, int(float64(g.Character.XShift)*1.2), int(float64(g.Character.YShift)*1.2))
+	g.Character.Draw(screen, g.camera.X, g.camera.Y)
 	if configuration.Global.DebugMode {
 		g.drawDebug(screen)
 	}
@@ -34,7 +34,6 @@ func (g Game) drawDebug(screen *ebiten.Image) {
 	gridLineSize := 2
 	cameraColor := color.NRGBA{R: 255, G: 0, B: 0, A: 255}
 	cameraLineSize := 1
-
 	mouseX, mouseY := ebiten.CursorPosition()
 
 	xMaxPos := configuration.Global.ScreenWidth
@@ -83,5 +82,5 @@ func (g Game) drawDebug(screen *ebiten.Image) {
 	ebitenutil.DebugPrintAt(screen, fmt.Sprint("(", g.camera.X, ",", g.camera.Y, ")"), xMaxPos+2*configuration.Global.TileSize+configuration.Global.TileSize/2, ySpace)
 
 	ebitenutil.DebugPrintAt(screen, "Character:", xMaxPos+2*configuration.Global.TileSize, 3*ySpace)
-	ebitenutil.DebugPrintAt(screen, fmt.Sprint("(", g.character.X, ",", g.character.Y, ")"), xMaxPos+2*configuration.Global.TileSize+configuration.Global.TileSize/2, 4*ySpace)
+	ebitenutil.DebugPrintAt(screen, fmt.Sprint("(", g.Character.X, ",", g.Character.Y, ")"), xMaxPos+2*configuration.Global.TileSize+configuration.Global.TileSize/2, 4*ySpace)
 }
