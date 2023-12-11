c244eaea07ed509618e1b2facc296debc2af4669
Pol Lamothe <lamothepol@gmail.com>
2023-12-11 20:30:07 +0100

    Optimisation de la génération infinie

--- quadtree/make.go
+++ quadtree/make.go
@@ -2,27 +2,26 @@ package quadtree
 
 import (
 	"gitlab.univ-nantes.fr/jezequel-l/quadtree/configuration"
+	"math/rand"
 )
 
 // MakeFromArray construit un quadtree représentant un terrain
 // étant donné un tableau représentant ce terrain.
 
-func recur(position string, flootContent [][]int, parent node, initTopLeftX, initTopLeftY int) node {
+func Recur(position string, flootContent [][]int, parent node, initTopLeftX, initTopLeftY int) node {
 	var laNode node
 	var parentWidth int = parent.width
 	var parentHeight int = parent.height
 	var parentWidthHalf int = parentWidth
 	var parentHeightHalf int = parentHeight
-	var parentTopY int = parent.TopLeftY
-	var parentTopx int = parent.TopLeftX
-	if (parentWidth)%2 != 0 {
+	if (parentWidth)%2 != 0 { //si la largeur est impaire on augmente la largeur des node a Gauche et augmente le TopLeftX de ce de Droite
 		if position == "topLeft" || position == "bottomLeft" {
 			parentWidth += 1
 		} else {
 			parentWidthHalf += 1
 		}
 	}
-	if (parentHeight)%2 != 0 {
+	if (parentHeight)%2 != 0 { //si la hauteur est impaire on augmente la hauteur des node en Haut et augmente le TopLeftY de ce en Bas
 		if position == "topLeft" || position == "topRight" {
 			parentHeight += 1
 		} else {
@@ -30,18 +29,18 @@ func recur(position string, flootContent [][]int, parent node, initTopLeftX, ini
 		}
 	}
 	if position == "Root" {
-		laNode = node{TopLeftX: parentTopx, TopLeftY: parentTopY, content: -1, height: parentHeight, width: parentWidth}
+		laNode = node{TopLeftX: parent.TopLeftX, TopLeftY: parent.TopLeftY, content: -1, height: parentHeight, width: parentWidth}
 	} else if position == "topLeft" {
-		laNode = node{TopLeftX: parentTopx, TopLeftY: parentTopY, content: -1, width: parentWidth / 2, height: parentHeight / 2}
+		laNode = node{TopLeftX: parent.TopLeftX, TopLeftY: parent.TopLeftY, content: -1, width: parentWidth / 2, height: parentHeight / 2}
 	} else if position == "topRight" {
-		laNode = node{TopLeftX: parentTopx + parentWidthHalf/2, TopLeftY: parentTopY, content: -1, width: parentWidth / 2, height: parentHeight / 2}
+		laNode = node{TopLeftX: parent.TopLeftX + parentWidthHalf/2, TopLeftY: parent.TopLeftY, content: -1, width: parentWidth / 2, height: parentHeight / 2}
 	} else if position == "bottomLeft" {
-		laNode = node{TopLeftX: parentTopx, TopLeftY: parentTopY + parentHeightHalf/2, content: -1, width: parentWidth / 2, height: parentHeight / 2}
+		laNode = node{TopLeftX: parent.TopLeftX, TopLeftY: parent.TopLeftY + parentHeightHalf/2, content: -1, width: parentWidth / 2, height: parentHeight / 2}
 	} else if position == "bottomRight" {
-		laNode = node{TopLeftX: parentTopx + parentWidthHalf/2, TopLeftY: parentTopY + parentHeightHalf/2, content: -1, width: parentWidth / 2, height: parentHeight / 2}
+		laNode = node{TopLeftX: parent.TopLeftX + parentWidthHalf/2, TopLeftY: parent.TopLeftY + parentHeightHalf/2, content: -1, width: parentWidth / 2, height: parentHeight / 2}
 	}
 	var state bool = false
-	if !(laNode.width == 1 && laNode.height == 1) && len(flootContent) > 0 {
+	if !(laNode.width == 1 && laNode.height == 1) && !configuration.Global.GenerationInfinie { //on vérifie si tout les blocs dans le node actuelle sont les mêmes
 		var origin int
 		state = true
 		var flootContentX, flootContentY int = laNode.TopLeftY, laNode.TopLeftX
@@ -49,58 +48,33 @@ func recur(position string, flootContent [][]int, parent node, initTopLeftX, ini
 		origin = flootContent[flootContentY][flootContentX]
 		for i := YRange; i < YRange+laNode.height; i++ {
 			for x := XRange; x < XRange+laNode.width; x++ {
-				if flootContent[i][x] != origin {
+				if flootContent[i][x] != origin { //si jamais il y'a un des bloc dont le node actuelle qui n'est pas égale aux autres
 					state = false
 				}
 			}
 		}
 	}
 	if (laNode.width == 1 && laNode.height == 1) || (state) {
-		if configuration.Global.GenerationInfinie {
-
-		} else {
+		if !configuration.Global.GenerationInfinie { //on ne définie pas de valeur au bloc si on est en génération infinie car on les définiras lorsque ils seront chargés
 			laNode.content = flootContent[laNode.TopLeftY][laNode.TopLeftX]
+		} else {
+			laNode.content = rand.Intn(5)
 		}
-		laNode.topLeftNode = nil
-		laNode.topRightNode = nil
-		laNode.bottomLeftNode = nil
-		laNode.bottomRightNode = nil
-	} else {
-		var topLeftNode node = recur("topLeft", flootContent, laNode, initTopLeftX, initTopLeftY)
+	} else if len(flootContent) > 0 {
+		var topLeftNode node = Recur("topLeft", flootContent, laNode, initTopLeftX, initTopLeftY)
 		laNode.topLeftNode = &topLeftNode
-		if (laNode.width == 1 || laNode.height == 1) && (laNode.width != 1 || laNode.height != 1) {
+		if (laNode.width == 1 || laNode.height == 1) && (laNode.width != 1 || laNode.height != 1) { // si on a l'une des deux dimensions qui est égale a 1 mais pas les deux
 			if laNode.width <= 1 {
-				var bottomLeftNode node = recur("bottomLeft", flootContent, laNode, initTopLeftX, initTopLeftY)
-				laNode.topRightNode = &topLeftNode
-				laNode.bottomRightNode = &bottomLeftNode
-			} else {
-				var topRightNode node = recur("topRight", flootContent, laNode, initTopLeftX, initTopLeftY)
-				laNode.topRightNode = &topRightNode
-				if laNode.height > 1 {
-					var bottomRightNode node = recur("bottomRight", flootContent, laNode, initTopLeftX, initTopLeftY)
-					laNode.bottomRightNode = &bottomRightNode
-				} else {
-					laNode.bottomRightNode = &topRightNode
-				}
-			}
-			if laNode.height <= 1 {
-				var topRightNode node = recur("topRight", flootContent, laNode, initTopLeftX, initTopLeftY)
-				laNode.bottomLeftNode = &topLeftNode
-				laNode.bottomRightNode = &topRightNode
-			} else {
-				var bottomLeftNode node = recur("bottomLeft", flootContent, laNode, initTopLeftX, initTopLeftY)
+				var bottomLeftNode node = Recur("bottomLeft", flootContent, laNode, initTopLeftX, initTopLeftY)
 				laNode.bottomLeftNode = &bottomLeftNode
-				if laNode.width > 1 {
-					var bottomRightNode node = recur("bottomRight", flootContent, laNode, initTopLeftX, initTopLeftY)
-					laNode.bottomRightNode = &bottomRightNode
-				} else {
-					laNode.bottomRightNode = &bottomLeftNode
-				}
+			} else {
+				var topRightNode node = Recur("topRight", flootContent, laNode, initTopLeftX, initTopLeftY)
+				laNode.topRightNode = &topRightNode
 			}
 		} else {
-			var topRightNode node = recur("topRight", flootContent, laNode, initTopLeftX, initTopLeftY)
-			var bottomLeftNode node = recur("bottomLeft", flootContent, laNode, initTopLeftX, initTopLeftY)
-			var bottomRightNode node = recur("bottomRight", flootContent, laNode, initTopLeftX, initTopLeftY)
+			var topRightNode node = Recur("topRight", flootContent, laNode, initTopLeftX, initTopLeftY)
+			var bottomLeftNode node = Recur("bottomLeft", flootContent, laNode, initTopLeftX, initTopLeftY)
+			var bottomRightNode node = Recur("bottomRight", flootContent, laNode, initTopLeftX, initTopLeftY)
 			laNode.topRightNode = &topRightNode
 			laNode.bottomLeftNode = &bottomLeftNode
 			laNode.bottomRightNode = &bottomRightNode
@@ -111,7 +85,7 @@ func recur(position string, flootContent [][]int, parent node, initTopLeftX, ini
 func MakeFromArray(floorContent [][]int, width, height, TopLeftX, TopLeftY int) (q Quadtree) {
 	var Quad Quadtree = Quadtree{
 		Width: width, Height: height}
-	var Laroot node = recur("Root", floorContent, node{width: Quad.Width, height: Quad.Height, TopLeftX: TopLeftX, TopLeftY: TopLeftY}, TopLeftX, TopLeftY)
+	var Laroot node = Recur("Root", floorContent, node{width: Quad.Width, height: Quad.Height, TopLeftX: TopLeftX, TopLeftY: TopLeftY}, TopLeftX, TopLeftY)
 	Quad.Root = &Laroot
 	return Quad
 }
